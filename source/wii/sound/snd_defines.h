9ed08fd8bf0219e31b029728fd99d6b4c0fa7f3e
dantoine <dantoine@1f7193ac-38c1-11de-8e0c-53a27eea117e>
2010-07-19 21:01:52 +0000

    * Add sound dir (maybe could be removed later) * Add new libpngu directory.
    git-svn-id: http://wiituka.googlecode.com/svn/trunk@22 1f7193ac-38c1-11de-8e0c-53a27eea117e

---
+++ source/wii/sound/snd_defines.h
@@ -0,0 +1,31 @@
+/****************************************************************************
+ * Pituka - Nintendo Wii/Gamecube Port
+ *
+ * D_Skywalk Sep 2008
+ *
+ * SND Images
+ *
+ * 
+ * Under CC License, see license.txt
+ ***************************************************************************/
+
+
+#ifndef _wiituka_snd_h_
+#define _wiituka_snd_h_
+
+extern const u8 intro_mp3[];
+extern const u32 intro_mp3_size;
+
+extern const u8 bckg_a_mp3[];
+extern const u32 bckg_a_mp3_size;
+
+extern const u8 bckg_b_mp3[];
+extern const u32 bckg_b_mp3_size;
+
+extern const u8 bckg_c_mp3[];
+extern const u32 bckg_c_mp3_size;
+
+extern const u8 bckg_d_mp3[];
+extern const u32 bckg_d_mp3_size;
+
+#endif
